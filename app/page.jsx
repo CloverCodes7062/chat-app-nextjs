6b02766c1cebff05fbda67a28c872daeb72594b1
Stacy McCarrell <clovercodes7062@gmail.com>
2024-01-18 16:17:11 -0500

    starting work on nextjs chat app

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+
+export default function HomePage() {
+    return (
+        <div>
+            <h1>Placeholder Landing Page</h1>
+            <p>
+                Welcome to the placeholder landing page
+            </p>
+            <a href="/login" rel="noopener noreferrer">
+                Login
+            </a>
+            <a href="/register" rel="noopener noreferrer">
+                Register
+            </a>
+        </div>
+    );
+}
---
+++ app/register/page.jsx
@@ -0,0 +1,25 @@
+
+export default function Register() {
+    return (
+        <div>
+            <h1>Placeholder Register Page</h1>
+            <p>
+                Welcome to the Placeholder Register Page
+            </p>
+            <form>
+                <div className="input-box">
+                    <input></input>
+                </div>
+                <div className="input-box">
+                    <input></input>
+                </div>
+                <div className="input-box">
+                    <input></input>
+                </div>
+            </form>
+            <a href="/" rel="noopener noreferrer">
+                Back to Landing Page
+            </a>
+        </div>
+    );
+}
