9893fe4771f01bd01628c901577cc0bbaf7b18dd
Stacy McCarrell <clovercodes7062@gmail.com>
2024-01-23 18:05:00 -0500

    almost done with functionality

---
+++ app/api/getParticipantToken/route.js
@@ -0,0 +1,37 @@
+import { NextResponse } from "next/server";
+
+export async function GET(req, res) {
+    const basicAuth = Buffer.from(process.env.NEXT_PUBLIC_DYTE_ORGANIZATION_ID + ":" + process.env.NEXT_PUBLIC_DYTE_API_KEY).toString('base64');
+    const meetingId = req.nextUrl.searchParams.get('meetingId');
+    const participantName = req.nextUrl.searchParams.get('participantName');
+    const participantId = req.nextUrl.searchParams.get('participantId');
+
+    console.log(meetingId, participantName, participantId);
+
+    try {
+        const options = {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Basic ${basicAuth}`
+            },
+            body: JSON.stringify({
+                name: participantName,
+                preset_name: "group_call_participant",
+                custom_participant_id: participantId
+            })
+        };
+        
+        const response = await fetch(`https://api.dyte.io/v2/meetings/${meetingId}/participants`, options);
+        const data = await response.json();
+
+        if (response.ok) {
+            return new Response(JSON.stringify(data.data));
+        } else {
+            console.log('Error')
+            return new Response('ERROR');
+        }
+    } catch (error) {
+        return new Response('Internal Error');
+    }
+}
--- app/components/Meeting.jsx
+++ app/components/Meeting.jsx
@@ -2,50 +2,24 @@
 import React, { useEffect, useState } from "react";
 import { DyteMeeting } from "@dytesdk/react-ui-kit";
 import { useDyteClient, DyteProvider } from "@dytesdk/react-web-core";
+import { v4 as uuidv4 } from 'uuid';
 
 export default function Meeting() {
     const [meeting, initMeeting] = useDyteClient();
-
-    const [participantId, setParticipantId] = useState('');
     const [participantName, setParticipantName] = useState('');
 
     const meetingId = 'bbb1400d-b51a-4fc1-8ddf-44cd111bcade';
     const basicAuth = Buffer.from(process.env.NEXT_PUBLIC_DYTE_ORGANIZATION_ID + ":" + process.env.NEXT_PUBLIC_DYTE_API_KEY).toString('base64');
 
-    const getParticipantToken = async () => {
-
-        try {
-            const options = {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json',
-                  Authorization: `Basic ${basicAuth}`
-                },
-                body: JSON.stringify({
-                    name: "Mary Sue",
-                    preset_name: "group_call_participant",
-                    custom_participant_id: "string"
-                })
-              };
-              
-              const response = await fetch(`https://api.dyte.io/v2/meetings/${meetingId}/participants`, options)
-              const data = await response.json();
-
-              return data;
-
-        } catch (error) {
-            console.error('Failed', error);
-        }
-    };
 
     const handleJoinMeeting = async () => {
         try {
-            const response = await getParticipantToken();
-            console.log(response);
+            const response = await fetch(`/api/getParticipantToken?meetingId=${meetingId}&participantName=${participantName}&participantId=${uuidv4()}`);
+            const data = await response.json();
 
-            if (response.success) {
-                console.log(response.data);
-                const authToken = response.data.token;
+            if (response.ok) {
+                console.log(data);
+                const authToken = data.token;
 
                 initMeeting({
                     authToken: authToken,
@@ -70,13 +44,7 @@ export default function Meeting() {
             </div>)
             : 
             (<div>
-                <h1>Join a Dyte Meeting</h1>
-                <input
-                    type="text"
-                    placeholder="Participant ID"
-                    value={participantId}
-                    onChange={(e) => setParticipantId(e.target.value)}
-                />
+                <h1>Join a Meeting</h1>
                 <input
                     type="text"
                     placeholder="Participant Name"
